b38af4747a0c59e72671375ebb1486d383fd4f4b
Ilya Vysotsky <47083790+ilubb@users.noreply.github.com>
2020-02-10 07:04:42 +0300

    get ready server to deploy

--- server.js
+++ server.js
@@ -1,6 +1,7 @@
 const express = require("express");
 const mongoose = require("mongoose");
 const bodyParser = require("body-parser");
+const path = require("path");
 
 const app = express();
 
@@ -12,6 +13,14 @@ mongoose
   .catch(err => console.log(err));
 
 app.use("/items", require("./routes/items"));
+
+if (process.env.NODE_ENV === "production") {
+  app.use(express.static("client/build"));
+
+  app.get("*", (req, res) => {
+    res.sendFile(path.resolve(__dirname, "client", "build", "index.html"));
+  });
+}
 
 app.listen(process.env.PORT, () =>
   console.log(`Server started on port ${process.env.PORT}`)
